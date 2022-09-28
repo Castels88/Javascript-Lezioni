f7d6eb05e6fe4f03e7e3ab68f0313e83ba19eb5a
[elio] <[castels@hotmail.it]>
2022-09-28 21:28:12 +0100

    fine lezione

--- Features/4 parte.js
+++
@@ -1,5 +0,0 @@
-//template literals
-let hero = "batman"
-
-let temp = `"help me " ${hero}`;
-console.log(temp)// "help me " batman tramite appunto ${hero} e mettendo tutto dentro i `` posso andare a richiamare la variabile hero e associarla alla variabile temp
